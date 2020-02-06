ff0c0a0120403b895d893ac9ea4de0c04a96097a
antoine <antoine.doeraene@b12-consulting.com>
2020-02-06 22:10:42 +0100

    background-color of td hover

--- frontend/src/test/scala/frontend/AppTest.scala
+++ frontend/src/test/scala/frontend/AppTest.scala
@@ -2,7 +2,6 @@ package frontend
 
 import org.scalajs.dom.document
 import org.scalatest.FunSuite
-import slinky.web.ReactDOM
 
 class AppTest extends FunSuite {
 //  test("Renders without crashing") {
