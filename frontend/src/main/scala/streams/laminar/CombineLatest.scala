e20a0b862f9bd9637e9bd194dbd4e577bc32d557
antoine <antoine.doeraene@b12-consulting.com>
2020-02-08 09:51:29 +0100

    overload combine latest for signals

--- frontend/src/main/scala/streams/laminar/CombineLatest.scala
+++ frontend/src/main/scala/streams/laminar/CombineLatest.scala
@@ -1,6 +1,7 @@
 package streams.laminar
 
 import com.raquo.airstream.eventstream.EventStream
+import com.raquo.airstream.signal.Signal
 
 object CombineLatest {
 
@@ -12,13 +13,9 @@ object CombineLatest {
     * Emits when: either stream emits a value
     * Ends when: both streams have ended.
     */
-  def apply[L, R](left$ : EventStream[L], right$ : EventStream[R]): EventStream[(L, R)] = {
-    val leftAsLeft$ = left$.map(Left[L, R])
-    val rightAsRight$ = right$.map(Right[L, R])
-
-    val mergeEithers$ = EventStream.merge(leftAsLeft$, rightAsRight$)
-
-    mergeEithers$
+  def apply[L, R](left$ : EventStream[L], right$ : EventStream[R]): EventStream[(L, R)] =
+    EventStream
+      .merge(left$.map(Left[L, R]), right$.map(Right[L, R]))
       .fold[(Option[L], Option[R])]((Option.empty[L], Option.empty[R])) { (acc: (Option[L], Option[R]), value) =>
         value match {
           case Left(left)   => (Some(left), acc._2)
@@ -27,6 +24,15 @@ object CombineLatest {
       }
       .changes
       .collect { case (Some(left), Some(right)) => (left, right) }
-  }
+
+  def apply[L, R](left$ : Signal[L], right$ : Signal[R], startLeft: => L, startRight: => R): Signal[(L, R)] =
+    EventStream
+      .merge(left$.changes.map(Left[L, R]), right$.changes.map(Right[L, R]))
+      .fold((startLeft, startRight)) { (acc: (L, R), value) =>
+        value match {
+          case Left(left)   => (left, acc._2)
+          case Right(right) => (acc._1, right)
+        }
+      }
 
 }
